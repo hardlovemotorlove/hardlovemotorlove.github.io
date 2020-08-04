48d043d03dd2292fdabf6cfbf1b38f1fe9e6f529
hugo <hugo.faugeroux@mmibordeaux.com>
2020-08-04 20:16:16 +0200

    animate & scroll v2

--- js/script.js
+++ js/script.js
@@ -1,56 +1,65 @@
-console.clear();
+$(document).ready(function() {
+	smoothScroll();
+	loader();
+});
+
+
 // AUTOPLAY
 
 document.getElementById('vid').play();
 
 // Smooth scroll
 
-$(document).ready(function() {
-    $('.link_ancre').on('click', function() {
-        var page = $(this).attr('href');
-        var speed = 1000;
+function smoothScroll() {
+	    $('.link_ancre').on('click', function() {
+        let page = $(this).attr('href');
+        let speed = 1000;
         $('html, body').animate( { scrollTop: $(page).offset().top }, speed );
         return false;
     });
-});
+}
 
 
 // intro animation
 
-gsap.to('.loader', 2, {
-	delay: 1,
-	top: "-100%",
-	opacity: 0,
-	ease: Expo.easeInOut,
-});
-gsap.from('.content', {
-	delay: 2.2,
-	display: "none",
-});
-gsap.from('#accueil .reseaux .icone_res', {
-	delay: 3,
-	opacity: 0,
-	y: 20,
-	ease: Power4.easeInOut,
-	stagger: {
-		each: 0.1,
-	}
-});
-gsap.from(['#accueil h1','#accueil p','#accueil .illustration'], {
-	delay: 2.2,
-	opacity: 0,
-	ease: Power1.easeInOut,
-	stagger: .3,
-});
+function loader() {
+	let introLoader = gsap.timeline();
+
+	introLoader.to('.loader', 2, {
+		delay: .3,
+		top: "-100%",
+		opacity: 0,
+		ease: Expo.easeInOut,
+	})
+	.from('.content', {
+		display: "none",
+	})
+	.from(['#accueil h1','#accueil p','#accueil .illustration'], {
+		opacity: 0,
+		ease: Power1.easeInOut,
+		stagger: .25,
+	}, "<")
+	.from('#accueil .reseaux .icone_res', {
+		opacity: 0,
+		y: 20,
+		ease: Power4.easeInOut,
+		stagger: {
+			each: 0.1,
+		}
+	});
+}
+
 
 // video parallax 
 
-const tl = gsap.timeline();
+let tl = gsap.timeline();
 tl.to("#accueil .illustration", 1, {y: 50})
-tl.to("#accueil .illustration video", 1, {y: 100}, '<' );
+tl.to("#accueil .illustration video", 1, {y: 100}, '<' )
+tl.to("#accueil h1", {y: -25}, '<')
+tl.to("#accueil p", {opacity: 0}, '<');
 
-const controller = new ScrollMagic.Controller();
-const scene = new ScrollMagic.Scene({
+let controller = new ScrollMagic.Controller();
+let scene = new ScrollMagic.Scene({
 	triggerElement: 'section#accueil',
 	duration: $('section#accueil').height(),
 	triggerHook: 0,
